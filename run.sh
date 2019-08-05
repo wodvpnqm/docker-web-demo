a505ce263b5e2b42c78030471ca2fccbf700701d
wodvpnqm <qiumiaod@gmail.com>
2019-08-05 10:11:34 +0800

    test1

--- run.sh
+++ run.sh
@@ -10,4 +10,4 @@ docker run -d --name webdemo-8452  -p 8452:8452 lgj/webdemo:1.0
 
 docker ps -a | grep webdemo-8452
 #查看启动日志
-docker logs -f -t --tail 10  microblog-scheduler-web
+docker logs -f -t --tail 10  webdemo-8452
